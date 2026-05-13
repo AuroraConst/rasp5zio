4d562b1960c34d216e5b828cf44cb6c6388a62b4
AuroraConst <69015147+AuroraConst@users.noreply.github.com>
2026-05-13 06:54:49 -0400

    updated FirstTest

--- mqtt-client/src/test/scala/org/aurora/mqttclient/FirstTest.scala
+++ mqtt-client/src/test/scala/org/aurora/mqttclient/FirstTest.scala
@@ -10,8 +10,8 @@ class FirstTest extends AnyWordSpec with should.Matchers:
     "work like this" in {
       import org.aurora.mqttclient.datatypes.*
       MqttSubscriber.subscribe()
-      MasterBedroomHeatersSet.SceneType.Half ().publish()
-      MasterBedroomLightsSet.SceneType.Half().publish() 
+      MasterBedroomHeatersSet.SceneType.On ().publish()
+      MasterBedroomLightsSet.SceneType.On().publish() 
       Thread.sleep(2000)
     }
   }
