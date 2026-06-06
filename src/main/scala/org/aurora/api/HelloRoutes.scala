ad67d10b319f34cf6d06761ff8b0f4305cd6ad85
AuroraConst <69015147+AuroraConst@users.noreply.github.com>
2026-06-05 22:40:27 -0400

    ??

--- src/main/scala/org/aurora/api/HelloRoutes.scala
+++ src/main/scala/org/aurora/api/HelloRoutes.scala
@@ -63,6 +63,7 @@ object HelloRoutes:
         //  result   <-Handler.text(os.RelPath( s"$path").toString) //docsHandler()
          result1  <- {
           val basePathRevised = docsBasePath
+          val relPath = os.RelPath( "/")
           // val basePathRevised =  if(path.toString == "") {docsBasePath}
           //   else docsBasePath / os.RelPath("/")
           Handler.text(s"basePathRevised: $basePathRevised") //Handler.fromFile(finalPath.toIO  )
